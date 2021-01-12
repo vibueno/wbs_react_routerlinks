86f40ee382ccf20132f1679d902dfa1c6b877b6f
vibueno <20514260+vibueno@users.noreply.github.com>
2021-01-12 14:54:59 +0100

    Updates

---
+++ precache-manifest.fb834ec6d59194ca272990e79e0da89e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8a6ece89ced363070f239c1e22246c",
+    "url": "/wbs_react_router_links/index.html"
+  },
+  {
+    "revision": "651ea3658c066a3f77ab",
+    "url": "/wbs_react_router_links/static/css/main.4203108f.chunk.css"
+  },
+  {
+    "revision": "b6feecee0f9c6317ae2c",
+    "url": "/wbs_react_router_links/static/js/2.51324fb0.chunk.js"
+  },
+  {
+    "revision": "651ea3658c066a3f77ab",
+    "url": "/wbs_react_router_links/static/js/main.f17c657b.chunk.js"
+  },
+  {
+    "revision": "eda8cc1424797a929659",
+    "url": "/wbs_react_router_links/static/js/runtime~main.1e9b5ae0.js"
+  }
+]);
